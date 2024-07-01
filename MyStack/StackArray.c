79959c0dc1e6a0dea954c45989f9087be688e409
BanxBan <106825535+BanxBan@users.noreply.github.com>
2024-07-01 12:16:20 +0800

    Update StackArray.c

--- MyStack/StackArray.c
+++ MyStack/StackArray.c
@@ -8,29 +8,24 @@ typedef struct {
     int top;
 } StackArrayList;
 
-// Function to initialize the stack
 void initStack(StackArrayList *L) {
     L->top = -1;
 }
 
-// Function to create a new stack
 StackArrayList createStack() {
     StackArrayList stack;
     initStack(&stack);
     return stack;
 }
 
-// Function to check if the stack is empty
 bool isEmpty(StackArrayList s) {
     return s.top == -1;
 }
 
-// Function to check if the stack is full
 bool isFull(StackArrayList s) {
     return s.top == MAX_SIZE - 1;
 }
 
-// Function to push an element onto the stack
 bool stack_push(StackArrayList *s, int elem) {
     if (isFull(*s)) {
         printf("Stack Overflow\n");
@@ -40,7 +35,6 @@ bool stack_push(StackArrayList *s, int elem) {
     return true;
 }
 
-// Function to pop an element from the stack
 bool stack_pop(StackArrayList *s) {
     if (isEmpty(*s)) {
         printf("Stack Underflow\n");
@@ -49,10 +43,8 @@ bool stack_pop(StackArrayList *s) {
     (s->top)--;
     return true;
 }
-
-// Function to display the elements of the stack
 void display(StackArrayList s) {
-    int i; // Declare loop variable here
+    int i; 
     if (isEmpty(s)) {
         printf("Stack is empty\n");
         return;
@@ -64,11 +56,10 @@ void display(StackArrayList s) {
     printf("\n");
 }
 
-// Function to visualize the stack
 void visualize(StackArrayList s) {
     printf("Stack Visualization:\n");
     printf("Top of Stack\n");
-    int i; // Declare loop variable here
+    int i; 
     for (i = s.top; i >= 0; i--) {
         printf("| %d |\n", s.data[i]);
     }
